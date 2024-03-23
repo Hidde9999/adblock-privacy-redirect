53218395d74d7de175c8108179705adb7889d87b
Hidde <hiddebalestra@gmail.com>
2024-03-23 17:17:56 +0100

    youtube channel blocker

---
+++ js/content/yt.js
@@ -0,0 +1,55 @@
+const blockedChannels = [
+    // NOS
+    "NOS",
+    "NOS Sport",
+    "NOS op 3",
+    "NOS Jeugdjournaal",
+    "NOS Stories",
+    "NOS Nieuws van de Week",
+
+    "De Telegraaf",
+    "RTL Nieuws",
+    "BNR",
+
+    "Omroep PowNed",
+    "De Avondshow met Arjen Lubach | VPRO",
+
+    "Vandaag Inside",
+
+    "VRT NWS",
+
+    "The Telegraph"
+];
+
+const videoTitels = [
+    "Overtreders",
+    "Handhavers"
+];
+
+function onReady() {
+    console.log("DOM fully loaded and parsed");
+    setInterval(() => {
+        // const videoElements = document.querySelectorAll("#dismissible, #content-section, .style-scope.ytd-rich-grid-row");
+        const videoElements = document.querySelectorAll("#dismissible, #content-section, .style-scope.yt-horizontal-list-renderer, .style-scope.ytd-rich-grid-row");
+
+        videoElements.forEach(function (video) {
+            const channelLink = video.querySelector('#channel-name a, #info-section #text');
+            const videoTitle = video.querySelector('#video-title, #video-title .style-scope.ytd-video-renderer');
+            if (channelLink && blockedChannels.includes(channelLink.innerHTML)) {
+                video.remove()
+            }
+            if (videoTitle && includesTitels(videoTitle.innerHTML)){
+                video.remove()
+            }
+        });
+    }, 250);
+}
+if (document.readyState !== "loading") {
+    onReady(); // Or setTimeout(onReady, 0); if you want it consistently async
+} else {
+    document.addEventListener("DOMContentLoaded", onReady);
+}
+
+function includesTitels(title) {
+    return videoTitels.some(keyword => title.includes(keyword));
+}
